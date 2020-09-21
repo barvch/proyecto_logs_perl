6e27185a4a698caa430e83e5b5c0bb6341410799
Edgar <ragdeyo@gmail.com>
2020-09-21 08:45:34 -0500

    Final

--- Integracion/apache_eq2.pl
+++ Integracion/apache_eq2.pl
@@ -1,20 +1,104 @@
 #!/usr/bin/perl
 
+=encoding UTF-8
+
+=head1 NAME
+
+apache_eq2.pl - En este a archivo, se hace un parsing del log del servicio indicando (Apache/NGINX) dentro del archivo de 
+configuracion B<(apache_eq2.conf)> y se determinan las direcciones IP de los clientes que estan haciendo un ataque de 
+fuerza bruta.
+
+=head1 SYNOPSIS
+
+  Se llama al script que se carga el archivo de configuración "apache_eq2.conf". 
+  En este archivo se pueden configurar diferentes valores como el servicio a monitorear, el tiempo de bloqueo a cada ip, 
+  el log que se analizará y el número de intentos con el cual se bloqueará a un host.
+  ./apache_eq2.pl
+
+=head2 DESCRIPTION
+
+  Este es el script principal que se encarga de llamar al módulo de bloqueo de IP's, tener un comportamiento 
+  de "servicio" y estar monitoreando el log de que se le indicó en el archivo de "apache_eq2.conf" para 
+  bloquear efectivamente las ips que intenten hacer un ataque por fuerza bruta/diccionario.
+
+=over 12
+
+=item C<Obtencion de datos - Mecanica de lectura del log>
+
+Por cada ronda de monitoreo, se leen unicamente las nuevas lineas agregadas al log del servicio.  
+De las lineas obtenidas para la ronda actual, se lee linea por linea y se obtienen los siguientes datos pertinentes:
+B<Direccion IP> del Host que se conecta al servidor web ($log->{rhost}).
+B<Hora> (UTC) en la que que llega la peticion al servidor en el formato HH:MM:SS  ($log->{time}).
+B<Fecha> en la que llega la peticion al servidor web en formato DD/MM/AAAA ($log->{date}).
+
+=item C<Analisis de datos y deteccion de ataques> 
+
+La conjuncion de los 3 datos resultantes del parsing del log, se genera una llave para cada una de las lineas leidas con la siguiente nomenclatura:
+ [IP] - [HORA] - [FECHA]
+Por ejemplo:
+ 192.168.123.45 - 08:45:23 - 19/09/2020
+Dicha llave, es almacenada dentro de un hash y como valor asociado, el numero de ocurrencias que se vayan detectando de la llave a la hora de recorrer linea por linea del log.
+B<Cuando el numero de ocurrencias asociadas al timestamp excede el numero tope de intentos fallidos> indicados en el archivo de configuracion, se llama a la funcion encargada de bannear la IP.
+El monitoreo es realizado cada cierto tiempo, el cual puede ser indicado/modificado dentro del archivo de configuracion.
+
+=back
+
+=head2 Methods
+
+=over 12
+
+=item C<daemonize>
+
+Función encargada de dar al programa un comportamiento de servicio. No recibe ningún parámetro ni devuelve ningún valor.
+
+=item C<tstktsk>
+
+Función que se manda a llamar tras el término de ejecución del programa. Detecta si para terminar la ejecución se 
+hace mediante SIGTERM, si es verdadero guarda en los logs la fecha de finalización del programa, en caso contrario 
+no hace nada.
+No recibe ningún argumento ni devuelve ningún valor.
+
+=item C<leeConfiguracion>
+
+Función encargada de parsear el archivo de configuración para inicializar algunas variables globales correctamente.
+No recibe nada y devuelve un hash de hashes de los datos encontrados en el archivo de configuración.
+Las llaves de los primeros hashes son los valores que se encuentran entre corchetes (en el archivo de configuración),
+y los valores son los datos que se encuentran en dicha sección.
+
+=back
+
+=head1 LICENSE
+
+Este módulo fue creado bajo una licencia artística.
+Ver L<perlartistic>.
+
+=head1 AUTHOR
+
+Equipo2 - PBSI 14G
+
+=head1 SEE ALSO
+
+L<UnblockIP>
+
+=cut
+
+
 use Apache::Log::Parser;
 use Time::Local;
-use Data::Dumper;
 use lib '/opt/apache_eq2/';
 use IPBlocker;
-use feature qw(say);
-use Data::Printer;
 
+#Función encargada de relizar la tarea para volver el script un servicio
+#Crea un fork y sólo el PID child queda vivo. Se cambia al directorio de trabajo del programa en /opt/apache_eq2/
+#y envía la salida estandar, entrada estándar y salida del error estándar a /dev/null para quitar la interacción
+#con la shell.
 sub daemonize {
    use POSIX;
    POSIX::setsid or die "setsid: $!";
    my $pid = fork() // die $!;
    exit(0) if $pid;
    #Directorio de trabajo.
-   chdir "/opt/apache_eq2";
+   chdir "/opt/apache_eq2/";
    umask 0;
    for (0 .. (POSIX::sysconf (&POSIX::_SC_OPEN_MAX) || 1024))
    { POSIX::close $_ }
@@ -25,6 +109,15 @@ sub daemonize {
 
 daemonize();
 
+$SIG{TERM} = \&tsktsk;
+
+sub tsktsk {
+    $SIG{TERM} = \&tsktsk;           
+    open LOG, ">>".$logService or die "No se pudo escribir en la bitácora"; 
+    print LOG "SERVICIO DETENIDO ".(strftime "%F %T", localtime)."\n";
+    close(LOG);
+    exit;
+}
 
 # Lectura de los valores del archivo de configuración
 sub leeConfiguracion {
@@ -54,6 +147,7 @@ sub leeConfiguracion {
 }
 
 my %valores = leeConfiguracion();
+
 
 if (exists (${$valores{"apache"}}{"log"})) {
    $logService = ${$valores{"apache"}}{"log"};
